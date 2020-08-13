bac91008a0c086e561386dfb4505377f4026673c
kriswithank <kristopherswann@gmail.com>
2020-08-13 18:49:17 -0500

    Other stuff

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-api-dev-attachmentsbucket-1ckvp1hwvwdha"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://urig3g4ixi.execute-api.us-east-1.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_ySiKVbAvW",
+    APP_CLIENT_ID: "2mncfjeuuotoj91hcaqe1vmnq2",
+    IDENTITY_POOL_ID: "us-east-1:5a39ceec-9bd4-4ada-a488-56b96fa83541"
+  }
+};
